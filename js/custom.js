a299c5d0238fb2a99981c0c226a05fc82a8f409b
Kadao <ayaka.shusuke@gmail.com>
2020-07-30 12:58:34 +1200

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+console.log('working');
+
+// Original js has been deleted
+// Refer to written notes below
+
+// **Revision**
+// *API revision needed
+// *Array revision needed
+// *(Two) Plugin revision needed
+// *Minified CSS file revision needed
+
+// **Code Issues**
+// *Issue where content would vanish upon activation
+// *Issue where input feedback would not log correctly
+// *Issue where placeholder name would sometimes not appear
